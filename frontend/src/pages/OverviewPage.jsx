3c7abacabcc1353e7ffff51201ca302b457113c3
thisumdev <thisumebuss@gmail.com>
2025-08-31 22:56:12 +0530

    Backend-frontend-basic-setup

---
+++ frontend/src/pages/OverviewPage.jsx
@@ -0,0 +1,7 @@
+export default function OverviewPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-100">
+      <h1 className="text-3xl font-semibold">Welcome to MovieRazzi 🎬</h1>
+    </div>
+  );
+}
