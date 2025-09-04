9a6128d292f5ac172caf40d4b8f30c316f7eb614
christine-samandi <christinefernando816@gmail.com>
2025-09-04 23:41:39 +0530

    Fix import statement for preference routes in main application file

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.database import Base, engine
 from auth import auth_routes
 from core.dependencies import get_current_user
+from routes import preference_routes
 
 
 app = FastAPI()
@@ -19,9 +20,9 @@ allow_methods=["*"],
 allow_headers=["*"],
 )
 
-
+#Include routers
 app.include_router(auth_routes.router)
-
+app.include_router(preference_routes.router)
 
 @app.get("/profile")
 def get_profile(username: str = Depends(get_current_user)):
